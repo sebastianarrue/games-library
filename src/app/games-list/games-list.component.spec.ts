3e0d66e1632511841cf94f39b143c049a7648c5d
sebastianarrue <sebasdap97@gmail.com>
2022-07-07 11:54:09 -0500

    Unit tests

--- src/app/games-list/games-list.component.spec.ts
+++ src/app/games-list/games-list.component.spec.ts
@@ -1,4 +1,10 @@
+import 'zone.js';
+import 'zone.js/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
 
 import { GamesListComponent } from './games-list.component';
 
@@ -6,18 +12,28 @@ describe('GamesListComponent', () => {
   let component: GamesListComponent;
   let fixture: ComponentFixture<GamesListComponent>;
 
+  beforeAll(async () => {
+    await TestBed.initTestEnvironment(
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting()
+    );
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GamesListComponent ]
-    })
-    .compileComponents();
+      declarations: [GamesListComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GamesListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', async () => {
     expect(component).toBeTruthy();
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const h1 = compiled.querySelector('p');
+    expect(h1.textContent).toContain('games-list works!');
   });
 });
--- src/app/red-flags/red-flags.component.spec.ts
+++ src/app/red-flags/red-flags.component.spec.ts
@@ -1,4 +1,10 @@
+import 'zone.js';
+import 'zone.js/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
 
 import { RedFlagsComponent } from './red-flags.component';
 
@@ -6,18 +12,24 @@ describe('RedFlagsComponent', () => {
   let component: RedFlagsComponent;
   let fixture: ComponentFixture<RedFlagsComponent>;
 
+  beforeAll(async () => {
+    await TestBed.initTestEnvironment(
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting()
+    );
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RedFlagsComponent ]
-    })
-    .compileComponents();
+      declarations: [RedFlagsComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RedFlagsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
   });
 });
